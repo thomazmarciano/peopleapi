be14e386318c6a846702faf078b14c9e4f0001e6
Thomaz Braga <thomazmarciano@gmail.com>
2021-08-30 18:14:02 -0300

    Developed listAll method

--- src/main/java/one/digitalinnovation/personapi/service/PersonService.java
+++ src/main/java/one/digitalinnovation/personapi/service/PersonService.java
@@ -9,6 +9,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 @Service
 public class PersonService {
 
@@ -26,5 +29,12 @@ public class PersonService {
 
         Person savedPerson = personRepository.save(personToSave);
         return MessageResponseDTO.builder().message("Create person with ID " + savedPerson.getId()).build();
+    }
+
+    public List<PersonDTO> listAll() {
+        List<Person> allPeople = personRepository.findAll();
+        return allPeople.stream()
+                .map(personMapper::toDTO)
+                .collect(Collectors.toList());
     }
 }
